dea01583a29ad423f4f8eae592d0fc5b9be4e860
Jorge Sandoval <jorgesagt5@gmail.com>
2026-02-10 12:18:31 -0700

    Se construyó la estructura para la aplicación

---
+++ src/main/java/jorge/web/app/sigmaBank/SigmaBankApplication.java
@@ -0,0 +1,13 @@
+package jorge.web.app.sigmaBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigmaBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigmaBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jorge/web/app/sigmaBank/SigmaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package jorge.web.app.sigmaBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigmaBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
