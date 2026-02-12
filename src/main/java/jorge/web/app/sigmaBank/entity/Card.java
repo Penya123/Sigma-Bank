fd8b74cb99fd45b6be5d84392ac73812dcf43b02
Jorge Sandoval <jorgesagt5@gmail.com>
2026-02-11 21:16:10 -0700

    Se terminó la clase Card

--- src/main/java/jorge/web/app/sigmaBank/entity/Card.java
+++ src/main/java/jorge/web/app/sigmaBank/entity/Card.java
@@ -1,14 +1,32 @@
 package jorge.web.app.sigmaBank.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 public class Card {
     private @Id @GeneratedValue(strategy = GenerationType.UUID)
-    Long cardId;
+    String cardId;
 
+    @Column(nullable = false, unique = true)
+    private Long cardNumber;
+
+    private long cardHolder;
+    private double balance;
+
+    @CreationTimestamp
+    private LocalDate iss;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime exp;
+    private String cvv;
+    private String pin;
+    private String billingAddress;
     
 }
